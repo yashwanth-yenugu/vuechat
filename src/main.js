be2a716bb1bb67f1686d3415c7ede662fec60510
Yashwanth Reddy <yashwanth.reddy@abjayonconsulting.com>
2019-04-09 15:31:36 +0530

    base commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
+var config = {
+  apiKey: "AIzaSyDcS0gaBnnGotQa41xiVWANpQszcW4vYRE",
+  authDomain: "chatwithvue.firebaseapp.com",
+  databaseURL: "https://chatwithvue.firebaseio.com",
+  projectId: "chatwithvue",
+  storageBucket: "chatwithvue.appspot.com",
+  messagingSenderId: "11958014385"
+};
+firebase.initializeApp(config);
+
+export const db = firebase.firestore()
 
 new Vue({
   router,
